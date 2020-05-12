113f00745da16f39ba5d4341b6b0f437acd1ac1c
Genaro Velazquez <genaro.velazquez@islas.org.mx>
2020-05-12 12:53:50 -0700

    Refactoriza variables siguiendo guía de estilo

--- tests/testthat/test_make_fit.R
+++ tests/testthat/test_make_fit.R
@@ -2,10 +2,10 @@ source("../../src/functions/make_fit.R")
 
 test_that('ajuste lineal', {
     in_file <- "../../reports/tables/noisy_data.csv"
-    resultados <- read.csv(in_file)
-    bayesian <- make_fit(resultados)
-    predicted_intercept <- as.numeric(bayesian[2])
+    results <- read.csv(in_file)
+    bayesian_fit <- make_fit(results)
+    predicted_intercept <- as.numeric(bayesian_fit[2])
     expect_equal(predicted_intercept,5.0,tolerance=0.05)
-    predicted_slope <- as.numeric(bayesian[4])
+    predicted_slope <- as.numeric(bayesian_fit[4])
     expect_equal(predicted_slope,2.0,tolerance=0.05)
 })
