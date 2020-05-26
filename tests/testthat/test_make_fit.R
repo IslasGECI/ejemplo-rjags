405cfe7355f539017a0be2160b4cd95d982c5036
Genaro Velazquez <genaro.velazquez@islas.org.mx>
2020-05-26 10:51:04 -0700

    Atiene correcciones de NR sobre guía de estilo

--- tests/testthat/test_make_fit.R
+++ tests/testthat/test_make_fit.R
@@ -1,11 +1,16 @@
 source("../../src/functions/make_fit.R")
 
+in_file <- "../../reports/tables/noisy_data.csv"
+results <- read.csv(in_file)
+bayesian_fit <- make_fit(results)
 test_that('ajuste lineal', {
-    in_file <- "../../reports/tables/noisy_data.csv"
-    results <- read.csv(in_file)
-    bayesian_fit <- make_fit(results)
     predicted_intercept <- as.numeric(bayesian_fit[2])
-    expect_equal(predicted_intercept,5.0,tolerance=0.05)
+    expect_equal(predicted_intercept, 
+                 5.0,
+                 tolerance = 0.05)
     predicted_slope <- as.numeric(bayesian_fit[4])
-    expect_equal(predicted_slope,2.0,tolerance=0.05)
-})
+    expect_equal(predicted_slope,
+                 2.0,
+                 tolerance = 0.05)
+    }
+)
