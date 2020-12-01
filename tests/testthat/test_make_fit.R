2d2bd038faa40c6234f6a21ab69909a5739e25bb
Nepo Rojas <braulio.rojas@islas.org.mx>
2020-12-01 12:20:33 -0800

    ⚗✏️ Corrige error de dedo
    Tuvimos que agregar la paquetería para que detectara las funciones

--- tests/testthat/test_make_fit.R
+++ tests/testthat/test_make_fit.R
@@ -4,14 +4,14 @@ library(geci.rjags)
 in_file <- "tests/data/noisy_data.csv"
 results <- read.csv(in_file)
 bayesian_fit <- make_fit(results)
-test_that("ajuste lineal", {
+testthat::test_that("ajuste lineal", {
   predicted_intercept <- as.numeric(bayesian_fit[2])
-  expect_equal(predicted_intercept,
+  testthat::expect_equal(predicted_intercept,
     5.0,
     tolerance = 0.05
   )
   predicted_slope <- as.numeric(bayesian_fit[4])
-  expect_equal(predicted_slope,
+  testthat::expect_equal(predicted_slope,
     2.0,
     tolerance = 0.05
   )
